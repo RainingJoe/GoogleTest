2cb8ce76f92c45bbdae1f624e21c43fcb6876385
RainingJoe <2273472696@qq.com>
2022-07-04 07:25:03 -0700

    google test demo

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/raining/Learning/GoogleTest
+# Build directory: /home/raining/Learning/GoogleTest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Test "MyTests")
+set_tests_properties(Test PROPERTIES  _BACKTRACE_TRIPLES "/home/raining/Learning/GoogleTest/CMakeLists.txt;10;add_test;/home/raining/Learning/GoogleTest/CMakeLists.txt;0;")
